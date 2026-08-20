deef59b5ae1ceca4377ffc70bc552c05de422eda
Bamba <bamba.df00@gmail.com>
2026-08-20 12:33:43 +0000

    reorganisation des fichier et dossier decomposition des page et widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home/home.dart';
-import 'login.dart';
+// import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,69 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'MyApp Flutter',
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "MyApp",
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
-        ),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MyLogin()),
-              );
-            },
-            icon: Icon(Icons.person),
-          ),
-        ],
-      ),
-      body: MyHome(),
-      bottomNavigationBar: MyBottomBr(),
-    );
-  }
-}
-
-class MyBottomBr extends StatefulWidget {
-  const MyBottomBr({super.key});
-
-  @override
-  State<MyBottomBr> createState() => _MyBottomBrState();
-}
-
-class _MyBottomBrState extends State<MyBottomBr> {
-  @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      selectedItemColor: Colors.lightBlue,
-      unselectedItemColor: Colors.black,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Aceuil'),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.production_quantity_limits),
-          label: 'Produits',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.receipt_long),
-          label: 'Aceuil',
-        ),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-      ],
+      title:'MyFirstApp',
+      // routerConfig: AppRoutes,
     );
   }
 }
